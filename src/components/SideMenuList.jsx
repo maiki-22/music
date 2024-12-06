19701bc4ec1f43ec3f25af53010a81c45d4ef032
maiki-22 <miguel.cortes.sandoval23@gmail.com>
2024-12-05 22:56:59 -0300

    aaaa

--- src/components/SideMenuList.jsx
+++ src/components/SideMenuList.jsx
@@ -6,10 +6,10 @@ export function SideMenuList() {
 
     return (
         <ul className="flex flex-col gap-2">
-            <SideMenuItem key="artists" href="music/add-artist" children={<><ArtistIcon /> Add Artists</>} />
-            <SideMenuItem key="albums" href="music/add-album" children={<><AlbumIcon /> Add Albums</>} />
-            <SideMenuItem key="tracks" href="music/add-track" children={<><TrackIcon /> Add Tracks</>} />
-            <SideMenuItem key="playlists" href="music/add-playlist" children={<><PlaylistIcon /> Add Playlists</>} />
+            <SideMenuItem key="artists" href="/add-artist" children={<><ArtistIcon /> Add Artists</>} />
+            <SideMenuItem key="albums" href="/add-album" children={<><AlbumIcon /> Add Albums</>} />
+            <SideMenuItem key="tracks" href="/add-track" children={<><TrackIcon /> Add Tracks</>} />
+            <SideMenuItem key="playlists" href="/add-playlist" children={<><PlaylistIcon /> Add Playlists</>} />
         </ul>
     );
 }
